026ebe90d3acdaafe0be6b7864696d362a2e82a5
sbenalca <sbenalca@espol.edu.ec>
2022-04-27 09:45:05 -0500

    Add Models and Tests for Tasks

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from core.models import Machine, MachineState, BranchOffice
+from backend.core.models import Employee, Task, TaskState, TaskType, Machine, MachineState, BranchOffice
 import datetime
 
-
+# Create your tests here.
 class MachineModelTest(TestCase):
     def setUp(self):
         s = MachineState.objects.create(label="Free")
@@ -13,3 +13,13 @@ class MachineModelTest(TestCase):
         """Machine creation succeeds test"""
         m = Machine.objects.get(serial_number="0950562199")
         self.assertEqual(m.serial_number,"0950562199")
+
+class TaskModelTest(TestCase):
+    def setUp(self):
+        b = BranchOffice.objects.create(name="First Office",address="Av. Victor Emilio Estrada",city="Guayaquil")
+        e = Employee.objects.create(id_number="0952796159",name="Sebastian",lastname="Benalcazar",address="Av. Victor Emilio Estrada",email="sbenalca@espol.edu.ec",branch=b)
+        ms = MachineState.objects.create(label="Free")
+        m = Machine.objects.create(serial_number="0950562199",model="A50",brand="Samsung",purchase_date=datetime.datetime.now(),state=ms,branch=b)
+        ts = TaskState.objects.create(label="estado")
+        tt = TaskType.objects.create(label="Corte")
+        t = Task.objects.create(description="Esto es un corte", start_datetime=datetime.datetime.now(), end_datetime = datetime.datetime.now(), duration = 60, state=ts, id_machine=m)
