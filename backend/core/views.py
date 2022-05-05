1064155f4b930ce0cba18b6f187bf6f551c33e9d
Nicole Agila <nagila@espol.edu.ec>
2022-05-05 11:08:12 -0500

    machine, task type, maintenance period and task views

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import MachineSerializer, TaskSerializer, TaskTypeSerializer, MaintenancePeriodSerializer
+from .models import Machine, Task, TaskType, MaintenancePeriod
 
-# Create your views here.
+
+class MachineViewSet(viewsets.ModelViewSet):
+    queryset = Machine.objects.all().order_by('serial_number')
+    serializer_class = MachineSerializer
+
+class TaskTypeViewSet(viewsets.ModelViewSet):
+    queryset = TaskType.objects.all().order_by('id')
+    serializer_class = TaskTypeSerializer
+
+class MaintenancePeriodViewSet(viewsets.ModelViewSet):
+    queryset = MaintenancePeriod.objects.all().order_by('id')
+    serializer_class = MaintenancePeriodSerializer
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all().order_by('id')
+    serializer_class = TaskSerializer
