7481d3f147a5f8f57d1bcae2c6b4e06f09d39603
Nicole Agila <nagila@espol.edu.ec>
2022-05-05 11:07:35 -0500

    correct foreign key

--- backend/core/models.py
+++ backend/core/models.py
@@ -55,7 +55,7 @@ class Task(models.Model):
     end_datetime = models.DateTimeField()
     duration = models.PositiveSmallIntegerField()
     state = models.ForeignKey('TaskState', on_delete=models.CASCADE)
-    id_machine = models.ForeignKey("Machine")
+    id_machine = models.ForeignKey("Machine", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.description
@@ -81,10 +81,10 @@ class TaskDetails(models.Model):
     invoice_num = models.CharField(max_length=30)
     espec_file_url = models.CharField(max_length=300)
     cuts = models.PositiveSmallIntegerField()
-    type = models.ForeignKey('TaskType')
-    id_task = models.ForeignKey("Task")
-    id_client = models.ForeignKey("Customer")
-    id_employee = models.ForeignKey("Employee")
+    type = models.ForeignKey('TaskType', on_delete=models.CASCADE)
+    id_task = models.ForeignKey("Task", on_delete=models.CASCADE)
+    id_client = models.ForeignKey("Customer", on_delete=models.CASCADE)
+    id_employee = models.ForeignKey("Employee", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.invoice_num
@@ -100,8 +100,8 @@ class MaintenanceDetails(models.Model):
     id = models.BigAutoField(primary_key=True)
     repetitions = models.PositiveSmallIntegerField()
     frecuency = models.PositiveSmallIntegerField()
-    period = models.ForeignKey("MaintenancePeriod")
-    id_task = models.ForeignKey("Task")
+    period = models.ForeignKey("MaintenancePeriod", on_delete=models.CASCADE)
+    id_task = models.ForeignKey("Task", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
@@ -116,8 +116,8 @@ class Priority(models.Model):
 class ReparationDetails(models.Model):
     id = models.BigAutoField(primary_key=True)
     reason = models.CharField(max_length=150)
-    priority = models.ForeignKey("Priority")
-    id_task = models.ForeignKey("Task")
+    priority = models.ForeignKey("Priority", on_delete=models.CASCADE)
+    id_task = models.ForeignKey("Task", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.id
