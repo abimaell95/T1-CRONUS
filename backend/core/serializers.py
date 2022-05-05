1a5b133e24a446b40d287c43f16126a7efc0a574
Nicole Agila <nagila@espol.edu.ec>
2022-05-05 11:09:45 -0500

    machine, task type, maintenance period and task class and serializer

---
+++ backend/core/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Machine, Task, TaskType, MaintenancePeriod
+
+class MachineSerializer(ModelSerializer):
+    class Meta:
+        model = Machine
+        fields = (
+            'serial_number', 'model', 'brand', 'purchase_date', 'created_date', 'state',
+            'branch'
+        )
+
+class TaskSerializer(ModelSerializer):
+    class Meta:
+        model = Task
+        fields = (
+            'id', 'description', 'start_datetime', 'end_datetime', 'duration',
+            'state', 'id_machine'
+        )
+
+class TaskTypeSerializer(ModelSerializer):
+    class Meta:
+        model = TaskType
+        fields = (
+            'id', 'label'
+        )
+
+class MaintenancePeriodSerializer(ModelSerializer):
+    class Meta:
+        model = MaintenancePeriod
+        fields = (
+            'id', 'label'
+        )
