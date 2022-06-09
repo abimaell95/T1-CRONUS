ba1ccd3612b861250724cb80f9e897aa4cf1ccec
Camilo Gutiérrez <37206064+abimaell95@users.noreply.github.com>
2022-06-09 09:50:15 -0500

    Adds User Serializer (#30)

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Machine
+from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.models import User
 
 class MachineSerializer(ModelSerializer):
     class Meta:
@@ -8,3 +10,20 @@ class MachineSerializer(ModelSerializer):
             'serial_number', 'model', 'brand', 'purchase_date', 'created_date', 'state',
             'branch'
         )
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
--- backend/cronus/urls.py
+++ backend/cronus/urls.py
@@ -15,17 +15,47 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from core import views
 from core.task import views as taskviews
+from django.contrib.auth.models import User
+from core.models import Customer
+from rest_framework import routers, serializers, viewsets
+from core.serializers import UserSerializer
+
 
 router = routers.DefaultRouter()
 router.register(r'task_type', taskviews.TaskTypeViewSet)
 router.register(r'maintenance_period', taskviews.MaintenancePeriodViewSet)
 
+class CustomerSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        return Customer.objects.create(**validated_data)
+
+    class Meta:
+        model = Customer
+        fields = ['id', 'name', 'surname', 'email']
+
+class CustomerViewSet(viewsets.ModelViewSet):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'customers', CustomerViewSet)
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include(router.urls)),
+    path('user',views.create_user),
+    path('admin/', admin.site.urls),
     path('machine/', views.MachineView.as_view()),
     path('task/', taskviews.TaskView.as_view()),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
