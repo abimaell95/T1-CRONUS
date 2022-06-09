93c1db4ec96991b21a8428859f2859e78e24de8d
Nicole Agila <nagila@espol.edu.ec>
2022-06-09 08:50:15 -0500

    admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Machine
-from .task.models import Task, TaskType, MaintenancePeriod
+from .task.models import Task, TaskType, MaintenancePeriod, MaintenanceDetails, ReparationDetails, TaskDetails
 
 admin.site.register(Machine)
 admin.site.register(Task)
 admin.site.register(TaskType)
 admin.site.register(MaintenancePeriod)
+admin.site.register(MaintenanceDetails)
+admin.site.register(ReparationDetails)
+admin.site.register(TaskDetails)
