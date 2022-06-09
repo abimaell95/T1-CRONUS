62c2d189f8685e96056b643e415fa60d1e490016
Nicole Agila <nagila@espol.edu.ec>
2022-06-09 09:00:04 -0500

    Feature/endpoints-na update  (#26)
    * correct foreign key
    
    * machine, task type, maintenance period and task views
    
    * machine, task type, maintenance period and task urls
    
    * machine, task type, maintenance period and task class and serializer
    
    * register machine, task type, maintenance period and task models
    
    * add rest_framework
    
    * extra files
    
    * machine endpoint by branch number
    
    * edit models to create joins
    
    * task folder
    
    * machine endpoint complete and task endpoint untested
    
    * changes
    
    * admin

--- backend/cronus/urls.py
+++ backend/cronus/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from core import views
+from core.task import views as taskviews
+
+router = routers.DefaultRouter()
+router.register(r'task_type', taskviews.TaskTypeViewSet)
+router.register(r'maintenance_period', taskviews.MaintenancePeriodViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('machine/', views.MachineView.as_view()),
+    path('task/', taskviews.TaskView.as_view()),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
