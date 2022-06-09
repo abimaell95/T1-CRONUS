398c9380a5f4f7a7e78120d9b699c214d9a9cce8
Nicole Agila <nagila@espol.edu.ec>
2022-06-09 09:00:50 -0500

    Revert "Feature/endpoints-na update  (#26)"
    This reverts commit 62c2d189f8685e96056b643e415fa60d1e490016.

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,11 +1,3 @@
 from django.contrib import admin
-from .models import Machine
-from .task.models import Task, TaskType, MaintenancePeriod, MaintenanceDetails, ReparationDetails, TaskDetails
 
-admin.site.register(Machine)
-admin.site.register(Task)
-admin.site.register(TaskType)
-admin.site.register(MaintenancePeriod)
-admin.site.register(MaintenanceDetails)
-admin.site.register(ReparationDetails)
-admin.site.register(TaskDetails)
+# Register your models here.
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,12 +1,3 @@
-from rest_framework import generics
-from .serializers import MachineSerializer
-from .models import Machine
+from django.shortcuts import render
 
-class MachineView(generics.ListAPIView):
-    serializer_class = MachineSerializer
-    def get_queryset(self):
-        queryset = Machine.objects.all()
-        branch_number = self.request.query_params.get('branch')
-        if branch_number is not None:
-            queryset = queryset.filter(branch=branch_number)
-        return queryset
+# Create your views here.
