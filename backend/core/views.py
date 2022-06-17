aa171c5aa9ebf3cd45da612434dca5151f8efa1d
Richard Cevallos apolo <ridaceva@espol.edu.ec>
2022-06-16 22:13:50 -0500

    Creacion de superusuario y de endpoint login en el que se autentica el usuario (primera version)

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+
+# esto desactiva los cors @csrf_exempt
+@csrf_exempt
+def login_view(request):
+    if request.method == "POST":
+        body = json.loads(request.body.decode('utf-8'))
+        user = authenticate(request, username=body["username"], password=body["password"])
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"error": False, "status": 200, "message": "Successful login"})
+        else:
+            return JsonResponse({"error": True, "status": 200, "message": "Invalid username and/or password."})
+    else:
+        return JsonResponse({"error": True, "status": 200, "message": "Invalid method"})
+
+
+class LoginView(View):
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request):
+        data = json.loads(request.body.decode('utf-8'))
+        user = authenticate(request, username=data["username"], password=data["password"])
+        if user is not None:
+            login(request, user)
+            return JsonResponse({"error": False, "status": 200, "message": "Successful login"})
+        else:
+            return JsonResponse({"error": True, "status": 200, "message": "Invalid username and/or password."})
+
+    def get(self, request):
+        return JsonResponse({"error": True, "status": 200, "message": "Invalid method"})
