5635654bbb980489f88106bcedfb05d5971d64e1
abimaell95 <gcemas20@gmail.com>
2022-06-20 22:50:08 -0500

    Adds App component to index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CreateOrder from './ui/components/CreateOrder';
+import App from './ui/components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CreateOrder />
+    <App />
   </React.StrictMode>
 );
