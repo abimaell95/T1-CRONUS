1bbc2ab4fe07a4a5b756e70db7df226b89217914
Nicole Agila <nagila@espol.edu.ec>
2022-07-02 01:42:27 -0500

    flake 8 and complete test for get orderbyid, get orders and get workflowbyid

--- backend/core/task/tests.py
+++ backend/core/task/tests.py
@@ -1,16 +1,14 @@
 import json
-from rest_framework.test import APITestCase
 from rest_framework import status
 from django.test import TestCase
 import datetime
-from core.models import BranchOffice, Employee
-from core.task.models import Event, EventState, EventType
 from core.models import Machine, MachineType,\
     MachineState, Employee, BranchOffice
 from core.task.models import Event, OrderDetails, EventState, EventType
 from core.workflow.models import MachineWorkflowStep, StepState
 
-class EventJoinStateTestSetUp(APITestCase):
+
+class EventJoinStateTestSetUp(TestCase):
 
     def setUp(self):
         self.employee = Employee.objects.create(
@@ -27,9 +25,9 @@ class EventJoinStateTestSetUp(APITestCase):
         )
         self.branch_office = BranchOffice.objects.create(
             id=1,
-            name= 'Sucursal 1',
-            address= 'Alborada 3ra etapa',
-            city= 'Daule'
+            name='Sucursal 1',
+            address='Alborada 3ra etapa',
+            city='Daule'
         )
         self.event_type = EventType.objects.create(
             id=1,
@@ -68,7 +66,7 @@ class EventJoinStateTestSetUp(APITestCase):
         self.event = Event.objects.create(
             id=4,
             description='tarea4',
-            start_datetime= '2022-06-15 06:00:00',
+            start_datetime='2022-06-15 06:00:00',
             end_datetime='2022-06-15 18:00:00',
             branch_id=1,
             employee_id=1,
@@ -98,7 +96,7 @@ class EventJoinStateTestSetUp(APITestCase):
         self.event = Event.objects.create(
             id=7,
             description='tarea6',
-            start_datetime= '2022-06-30 06:00:00',
+            start_datetime='2022-06-30 06:00:00',
             end_datetime='2022-06-30 18:00:00',
             branch_id=1,
             employee_id=1,
@@ -107,14 +105,15 @@ class EventJoinStateTestSetUp(APITestCase):
         )
 
     def test_run(self):
-        response=self.client.get('/api/events/?year=2022&month=06&day=01&branch=01&period=1')
+        get_str = '/api/events/?year=2022&month=06&day=01&branch=01&period=1'
+        response = self.client.get(get_str)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         responsedic = response.data["data"]
         for v in responsedic:
             for clave, valor in v.items():
                 if clave == 'start_datetime':
-                    self.assertEqual(valor.split("T")[0],"2022-06-01")
-        
+                    self.assertEqual(valor.split("T")[0], "2022-06-01")
+
 
 class OrderIdTest(TestCase):
     id = 1
@@ -177,10 +176,16 @@ class OrderIdTest(TestCase):
             state_id=1
         )
 
-    def test_run(self):
+    def test_ok(self):
         response = self.client.get("/api/order/?id={}".format(self.id))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["data"][0]["id"], self.id)
+
+    def test_notok(self):
+        incorrect_id = 10
+        response = self.client.get("/api/order/?id={}".format(incorrect_id))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data["data"], [])
 
 
 class AllOrderTest(TestCase):
@@ -189,7 +194,7 @@ class AllOrderTest(TestCase):
     year = "2022"
     branch2 = 2
     branch1 = 1
-    date = "{}-{}-{}".format(year,month,day)
+    date = "{}-{}-{}".format(year, month, day)
 
     def setUp(self) -> None:
         StepState.objects.create(id=1, label="No Iniciado")
@@ -282,33 +287,6 @@ class AllOrderTest(TestCase):
             order_id=2,
             state_id=1
         )
-        Event.objects.create(
-            id=3,
-            description='tarea3',
-            start_datetime='{}-{}-06 06:00:00'.format(self.year, self.month),
-            end_datetime='2022-06-06 18:00:00',
-            branch_id=1,
-            employee_id="0927643825",
-            state_id=1,
-            type_id=1
-        )
-        OrderDetails.objects.create(
-            id=3,
-            client_name="Carmen Pinto",
-            invoice_num="10001-20",
-            file_url="Archivo.jpg",
-            num_pieces=100,
-            current_step_id=1,
-            event_id=3
-        )
-        MachineWorkflowStep.objects.create(
-            id=3,
-            step_order=1,
-            end_datetime="2019-01-01 00:00:00",
-            machine_id="123456",
-            order_id=3,
-            state_id=1
-        )
         BranchOffice.objects.create(
             id=2,
             name='Sucursal 2',
@@ -362,43 +340,62 @@ class AllOrderTest(TestCase):
             state_id=1
         )
 
-    def test_branch(self):
+    def test_branchok(self):
         get_str = '/api/orders/?year={}&month={}&day={}&branch={}'.format(
             self.year, self.month, self.day, self.branch2)
         response = self.client.get(get_str)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        for i in response.data["data"]:
-            for k, v in i.items():
-                if k == 'branch_id':
-                    self.assertEqual(v, self.branch2)
+        branch = response.data["data"][0]["branch_id"]
+        self.assertEqual(branch, self.branch2)
 
-    def test_weekly(self):
+    def test_branch_notok(self):
+        not_existing_branch = 5
+        get_str = '/api/orders/?year={}&month={}&day={}&branch={}'.format(
+            self.year, self.month, self.day, not_existing_branch)
+        response = self.client.get(get_str)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data["data"], [])
+
+    def test_weekly_ok(self):
         get_str = '/api/orders/?year={}&month={}&day={}'.format(
             self.year, self.month, self.day)
         response = self.client.get(get_str)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        for i in response.data["data"]:
-            for k, v in i.items():
-                if k == 'start_datetime':
-                    low_date = datetime.datetime.strptime(
-                        '{}-{}-{} 00:00:00'.format(
-                            self.year, self.month, self.day
-                        ),
-                        '%Y-%m-%d %H:%M:%S'
-                    )
-                    up_date = datetime.datetime.strptime(
-                        '{}-{}-{} 23:59:59'.format(
-                            self.year, self.month, int(self.day) + 6
-                        ),
-                        '%Y-%m-%d %H:%M:%S'
-                    )
-                    date_str = v.replace("T", " ").replace("Z", "")
-                    order_date = datetime.datetime.strptime(
-                        date_str, '%Y-%m-%d %H:%M:%S'
-                    )
-                    condition = order_date > low_date and order_date < up_date
-                    self.assertEqual(condition, True)
- 
+        low_date = datetime.datetime.strptime(
+            '{}-{}-{} 00:00:00'.format(
+                self.year, self.month, self.day
+            ),
+            '%Y-%m-%d %H:%M:%S'
+        )
+        up_date = datetime.datetime.strptime(
+            '{}-{}-{} 23:59:59'.format(
+                self.year, self.month, int(self.day) + 6
+            ),
+            '%Y-%m-%d %H:%M:%S'
+        )
+        d1 = response.data["data"][0]["start_datetime"]
+        date_str = d1.replace("T", " ").replace("Z", "")
+        order_date = datetime.datetime.strptime(
+            date_str, '%Y-%m-%d %H:%M:%S'
+        )
+        condition = order_date > low_date and order_date < up_date
+        self.assertEqual(condition, True)
+        d2 = response.data["data"][1]["start_datetime"]
+        date_str = d2.replace("T", " ").replace("Z", "")
+        order_date = datetime.datetime.strptime(
+            date_str, '%Y-%m-%d %H:%M:%S'
+        )
+        condition = order_date > low_date and order_date < up_date
+        self.assertEqual(condition, True)
+
+    def test_weekly_notok(self):
+        incorrect_month = 12
+        get_str = '/api/orders/?year={}&month={}&day={}'.format(
+            self.year, incorrect_month, self.day)
+        response = self.client.get(get_str)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.data["data"], [])
+
     def test_orderPost(self):
         data = {'description': 'ewew',
                 'start_date': '2022-06-17',
@@ -410,20 +407,19 @@ class AllOrderTest(TestCase):
                 'invoice_num': 'fg',
                 'pieces_number': 0,
                 'plan_file': 'null',
-                'workflow':1}
-        response = self.client.post('/api/order/',json.dumps(data),content_type="application/json")
+                'workflow': 1}
+        response = self.client.post('/api/order/',
+                                    json.dumps(data),
+                                    content_type="application/json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         data = response.data.get('data')
         event = data.get('event')
-        print(event)
         order = data.get('order')
-        print(order)
 
         c_event = Event.objects.get(id=event.get('id'))
         self.assertEqual(str(c_event), event.get('description'))
         c_order = OrderDetails.objects.get(id=order.get('id'))
         self.assertEqual(str(c_order), str(order.get('id')))
-
 
     def test_availableHours(self):
         get_str = '/api/available_hours/?branch={}&date={}'.format(
